c0421fe294c431c71e7d83487a9f0790e05cbe8b
seanxcwang <seanxcwang@tencent.com>
2021-01-25 18:57:36 +0800

    [RKNPU] add softplus/tanh/upsample

---
+++ source/tnn/device/rknpu/convert/rknpu_upsample_layer.cc
@@ -0,0 +1,53 @@
+// Tencent is pleased to support the open source community by making TNN available.
+//
+// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#include <algorithm>
+
+#include "rknpu_base_layer.h"
+#include "rknpu_utils.h"
+
+namespace TNN_NS {
+
+DECLARE_RKNPU_LAYER(Upsample, LAYER_UPSAMPLE);
+
+Status RknpuUpsampleLayer::Convert() {
+    auto param = dynamic_cast<UpsampleLayerParam *>(param_);
+    CHECK_PARAM_NULL(param);
+
+    if (param->mode != 2 || param->align_corners != 0) {
+        return Status(TNNERR_PARAM_ERR, "upsample only support bilinear mode");
+    }
+
+    Status ret = TNN_OK;
+    std::vector<std::shared_ptr<rk::nn::Tensor>> inputs;
+
+    // input
+    inputs.push_back(input_ops_[0]);
+
+    // output
+    ADD_OUTPUT_OP();
+
+    rk::nn::UpsampleAttr attr;
+
+    auto output_dims = output_ops_[0]->GetDims();
+    attr.sizes.assign(output_dims.end() - 2, output_dims.end());
+
+    graph_->AddOperator(rk::nn::OperatorType::UPSAMPLE, inputs, output_ops_, (void *)&attr);
+
+    return ret;
+}
+
+REGISTER_RKNPU_LAYER(Upsample, LAYER_UPSAMPLE);
+
+}  // namespace TNN_NS
